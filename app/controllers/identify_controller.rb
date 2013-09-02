757121a2ac525ad2374f69cdcf3adbdc1d495cf2
Amaury Balmer <amaury@beapi.fr>
2013-09-03 01:32:04 +0200

    work on first method on controller

--- app/controllers/identify_controller.rb
+++ app/controllers/identify_controller.rb
@@ -3,14 +3,21 @@ class IdentifyController < ApplicationController
 
 
   def index
+  	@identifies = Identify.all
   end
 
   def add
+  	# TODO
   end
 
   def edit
+  	# TODO
   end
 
   def delete
+    identify = Identify.find(params[:id])
+    identify.delete()
+
+    redirect_to :action => 'index'
   end
 end
